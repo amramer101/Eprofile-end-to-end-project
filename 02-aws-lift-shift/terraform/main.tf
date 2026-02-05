2cfaae7ba6a298c01fb92fee18774c3dce912b7c
amr <amrmedhatamer100@gmail.com>
2026-02-05 23:30:09 +0200

    add nginx.sh script to user_data for EC2 instance and update security group rules to allow HTTP traffic from anywhere

--- 02-aws-lift-shift/terraform/main.tf
+++ 02-aws-lift-shift/terraform/main.tf
@@ -1,17 +1,25 @@
+### EC2 Instance for Nginx Server
+
 module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
+  source = "terraform-aws-modules/ec2-instance/aws"
 
   name = "Nginx-instance"
 
-  instance_type = "t3.micro"
+  instance_type               = "t2.micro"
   associate_public_ip_address = true
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI (HVM), SSD Volume Type
-  key_name      = "user1"
-  monitoring    = true
-  subnet_id     = module.vpc.public_subnets[0]
+  ami                         = "ami-01f79b1e4a5c64257" # eu-central-1 ubuntu 20.24.04 LTS
+  vpc_security_group_ids      = [aws_security_group.Frontend-SG.id]
+  key_name                    = aws_key_pair.EC2_Key_Pair.key_name
+  monitoring                  = false
+  subnet_id                   = module.vpc.public_subnets[0]
 
   tags = {
     Terraform   = "true"
     Environment = "dev"
   }
+
+  user_data = file("../../01-local-setup/Automated-Setup/nginx.sh")
+
 }
+
+### EC2 Instance for Tomcat Server
