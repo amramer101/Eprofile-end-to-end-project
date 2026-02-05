9f30a2e7d94c2fbf020ab031ab471df761964a3a
amr <amrmedhatamer100@gmail.com>
2026-02-05 22:51:31 +0200

    add ### Security Group for the Frontend EC2 Instance and update the security group rules to allow HTTP traffic on port 80 and all outbound traffic.

--- 02-aws-lift-shift/terraform/secgrp.tf
+++ 02-aws-lift-shift/terraform/secgrp.tf
@@ -11,7 +11,7 @@ resource "aws_security_group" "Frontend-SG" {
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4_frontend" {
   security_group_id = aws_security_group.Frontend-SG.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 22
@@ -27,14 +27,49 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   to_port           = 80
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_frontend" {
   security_group_id = aws_security_group.Frontend-SG.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-  security_group_id = aws_security_group.Frontend-SG.id
-  cidr_ipv6         = "::/0"
+
+
+
+### Security Group for the Tomcat EC2 Instance
+
+resource "aws_security_group" "Tomcat-SG" {
+  name        = "Tomcat-SG"
+  description = "Allow 8080 inbound traffic from Frontend and all outbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  tags = {
+    Name = "Allow 8080 from Frontend"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4_tomcat" {
+  security_group_id = aws_security_group.Tomcat-SG.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_8080_from_frontend" {
+  security_group_id            = aws_security_group.Tomcat-SG.id
+  referenced_security_group_id = aws_security_group.Frontend-SG.id
+  from_port                    = 8080
+  ip_protocol                  = "tcp"
+  to_port                      = 8080
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_tomcat" {
+  security_group_id = aws_security_group.Tomcat-SG.id
+  cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
+
+
+
+
