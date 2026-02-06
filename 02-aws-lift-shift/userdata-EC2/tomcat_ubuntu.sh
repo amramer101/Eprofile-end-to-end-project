e05fc84dc1133b84645d11c4110565c2d322f958
amr <amrmedhatamer100@gmail.com>
2026-02-06 18:48:56 +0200

    add Elasticsearch configuration to application.properties and update hostnames in application.properties and output.tf

--- 02-aws-lift-shift/userdata-EC2/tomcat_ubuntu.sh
+++ 02-aws-lift-shift/userdata-EC2/tomcat_ubuntu.sh
@@ -6,12 +6,12 @@ sudo apt install openjdk-17-jdk -y
 
 sudo apt install tomcat10 tomcat10-admin tomcat10-docs tomcat10-common git -y
 
-sudo apt install awscli -y
+sudo snap install aws-cli --classic
 
 sudo rm -rf /var/lib/tomcat10/webapps/ROOT
 
 echo "Downloading Artifact from S3..."
-aws s3 cp s3://S3-terraform-2026-java-artifacts1598/vprofile-v2.war /var/lib/tomcat10/webapps/ROOT.war
+aws s3 cp s3://s3-terraform-2026-java-artifacts1598/vprofile-v2.war /var/lib/tomcat10/webapps/ROOT.war
 
 sudo systemctl start tomcat10
 sudo systemctl enable tomcat10
