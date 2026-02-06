1125b54f0fc698cdaa75a28ed147a10345ee44e8
amr <amrmedhatamer100@gmail.com>
2026-02-06 16:43:07 +0200

    edit tomcat10 scritp to download from s3 bucket

--- 02-aws-lift-shift/userdata-EC2/tomcat_ubuntu.sh
+++ 02-aws-lift-shift/userdata-EC2/tomcat_ubuntu.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 sudo apt update
 sudo apt upgrade -y
+
 sudo apt install openjdk-17-jdk -y
+
 sudo apt install tomcat10 tomcat10-admin tomcat10-docs tomcat10-common git -y
+
+sudo apt install awscli -y
+
+sudo rm -rf /var/lib/tomcat10/webapps/ROOT
+
+echo "Downloading Artifact from S3..."
+aws s3 cp s3://S3-terraform-2026-java-artifacts1598/vprofile-v2.war /var/lib/tomcat10/webapps/ROOT.war
+
+sudo systemctl start tomcat10
+sudo systemctl enable tomcat10
+sudo systemctl restart tomcat10
