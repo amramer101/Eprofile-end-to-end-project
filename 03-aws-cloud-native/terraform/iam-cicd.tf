c05ac8a11c3a6d39beefd0f0633d5b9ba151b23b
amr <amrmedhatamer100@gmail.com>
2026-02-13 15:50:19 +0200

    deleted:    01-local-setup/src

--- 03-aws-cloud-native/terraform/iam-cicd.tf
+++ 03-aws-cloud-native/terraform/iam-cicd.tf
@@ -1,13 +1,14 @@
-### Role for CodeBuild
-
+# -----------------------------------------------------------
+# 1. Role for CodeBuild (صلاحيات كاملة لعملية البناء)
+# -----------------------------------------------------------
 resource "aws_iam_role" "codebuild_role" {
   name = "vprofile-codebuild-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
       Principal = { Service = "codebuild.amazonaws.com" }
     }]
   })
@@ -21,38 +22,26 @@ resource "aws_iam_role_policy" "codebuild_policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow"
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ]
+        # صلاحيات كاملة لكل شيء يحتاجه Build (Logs, S3, EC2)
+        Effect   = "Allow"
+        Action   = ["*"]
         Resource = "*"
-      },
-      {
-        Effect = "Allow"
-        Action = ["s3:GetObject", "s3:GetObjectVersion", "s3:PutObject"]
-        Resource = [
-          "${aws_s3_bucket.codepipeline_bucket.arn}",
-          "${aws_s3_bucket.codepipeline_bucket.arn}/*"
-        ]
       }
     ]
   })
 }
 
 # -----------------------------------------------------------
-
-### Role for CodePipeline
-
+# 2. Role for CodePipeline (صلاحيات كاملة لإدارة خط الإنتاج والرفع)
+# -----------------------------------------------------------
 resource "aws_iam_role" "codepipeline_role" {
   name = "vprofile-codepipeline-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
       Principal = { Service = "codepipeline.amazonaws.com" }
     }]
   })
@@ -65,32 +54,45 @@ resource "aws_iam_role_policy" "codepipeline_policy" {
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
-      {
-        Effect = "Allow"
-        Action = ["s3:GetObject", "s3:GetObjectVersion", "s3:GetBucketVersioning", "s3:PutObjectAcl", "s3:PutObject"]
-        Resource = [
-          "${aws_s3_bucket.codepipeline_bucket.arn}",
-          "${aws_s3_bucket.codepipeline_bucket.arn}/*"
-        ]
-      },
+      # 1. القضاء على مشاكل S3 نهائياً (بما فيها DeleteObject و GetBucketPolicy)
       {
         Effect   = "Allow"
-        Action   = ["codebuild:BatchGetBuilds", "codebuild:StartBuild"]
+        Action   = ["s3:*"]
         Resource = "*"
       },
+      # 2. القضاء على مشاكل Elastic Beanstalk نهائياً
       {
-        Effect = "Allow"
-        Action = [
-          "elasticbeanstalk:CreateApplicationVersion",
-          "elasticbeanstalk:DescribeApplicationVersions",
-          "elasticbeanstalk:UpdateEnvironment",
-          "elasticbeanstalk:DescribeEnvironments"
+        Effect   = "Allow"
+        Action   = ["elasticbeanstalk:*"]
+        Resource = "*"
+      },
+      # 3. صلاحيات كاملة لـ CodeBuild و CodeStar
+      {
+        Effect   = "Allow"
+        Action   = [
+          "codebuild:*",
+          "codestar-connections:*"
         ]
         Resource = "*"
       },
+      # 4. صلاحيات ضرورية للخدمات المرتبطة (EC2, ASG, RDS, CloudFormation)
       {
         Effect   = "Allow"
-        Action   = ["codestar-connections:UseConnection"]
+        Action   = [
+          "ec2:*",
+          "elasticloadbalancing:*",
+          "autoscaling:*",
+          "cloudwatch:*",
+          "sns:*",
+          "cloudformation:*",
+          "rds:*"
+        ]
+        Resource = "*"
+      },
+      # 5. صلاحية تمرير أي رول (PassRole) لضمان عدم توقف Beanstalk
+      {
+        Effect   = "Allow"
+        Action   = ["iam:PassRole"]
         Resource = "*"
       }
     ]
