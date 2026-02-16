9e9a318d13d1761d31ab85163000ac6334119f9b
amr <amrmedhatamer100@gmail.com>
2026-02-16 15:49:32 +0200

    try to make the stage 2 work without src folder and without application.properties file, by hardcoding the values in the tomcat_ubuntu.sh script.

--- 02-aws-lift-shift/userdata-EC2/tomcat_ubuntu.sh
+++ 02-aws-lift-shift/userdata-EC2/tomcat_ubuntu.sh
@@ -14,6 +14,15 @@ sudo rm -rf /var/lib/tomcat10/webapps/ROOT
 echo "Downloading Artifact from S3..."
 aws s3 cp s3://s3-terraform-2026-java-artifacts1598/vprofile-v2.war /var/lib/tomcat10/webapps/ROOT.war
 
+echo "Injecting Environment Variables for Stage 2..."
+cat <<EOF > /usr/share/tomcat9/bin/setenv.sh
+export RDS_HOSTNAME=db01.eprofile.in
+export RABBITMQ_HOSTNAME=rmq01.eprofile.in
+export MEMCACHED_HOSTNAME=mc01.eprofile.in
+EOF
+
+chmod +x /usr/share/tomcat9/bin/setenv.sh
+
 sudo systemctl start tomcat10
 sudo systemctl enable tomcat10
 sudo systemctl restart tomcat10
